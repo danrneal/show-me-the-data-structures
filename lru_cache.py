ae2749744c5b9bd5a67915d09a8d55a0e8f4d11b
Daniel R. Neal <dan.r.neal@gmail.com>
2020-05-29 15:23:11 -0700

    fix: LRU Cache now passes all test cases

--- lru_cache.py
+++ lru_cache.py
@@ -11,21 +11,21 @@ class LruCache:
     """Cache object that removes the least recently used when at capacity.
 
     Attributes:
-        cache: A dict representing the cache object
-        call_cache: A dict representing a cache of keys of the most recently
-            used resources
+        cache: A dict representing the cache object holding nodes to a double
+            linked list holding LruCacheNode objects
         capacity: An int representing the capacity of the cache
-        size: An int representing the number of elements currently in the cache
-        call_id: An int representing an incrementing number for order of calls
+        head: A LruCacheNode representing the most recently used object in the
+            cache and the beginning of a doubly linked list
+        tail: A LruCacheNode representing the least recently used object in the
+            cache and the beginning of a doubly linked list
     """
 
     def __init__(self, capacity):
         """Set-up for a least recently used cache."""
         self.cache = {}
-        self.call_cache = {}
         self.capacity = capacity
-        self.size = 0
-        self.call_id = 0
+        self.head = None
+        self.tail = None
 
     def get(self, key):
         """Retrieve a value from the cache.
@@ -39,10 +39,10 @@ class LruCache:
         if key not in self.cache:
             return -1
 
-        self.call_cache[self.call_id] = key
-        self.call_id += 1
+        node = self.cache[key]
+        self.update_most_recently_used(node)
 
-        return self.cache[key]
+        return node.value
 
     def put(self, key, value):
         """Place a value into the cache at the given key.
@@ -52,27 +52,80 @@ class LruCache:
                 cache
             value: An int representing the value to place into the cache
         """
-        if key not in self.cache:
-            if self.size == self.capacity:
-                self.remove()
-            self.size += 1
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self.update_most_recently_used(node)
+            return
 
-        self.cache[key] = value
-        self.call_cache[self.call_id] = key
-        self.call_id += 1
+        if len(self.cache) == self.capacity:
+            self.remove()
+
+        node = LruCacheNode(key, value, self.head)
+        self.cache[key] = node
+        self.head = node
+
+        if node.next is None:
+            self.tail = node
+        else:
+            node.next.prev = node
 
     def remove(self):
         """Remove the least recently used resource from the cache."""
-        call_id = self.call_id - self.capacity
-        key = self.call_cache[call_id]
-        del self.call_cache[call_id]
-        del self.cache[key]
+        del self.cache[self.tail.key]
+        self.tail = self.tail.prev
+
+        if self.tail is None:
+            self.head = None
+            return
+
+        self.tail.next.prev = None
+        self.tail.next = None
+
+    def update_most_recently_used(self, node):
+        """Move a node to the head of the doubly linked list.
+
+        Args:
+            node: A LruCacheNode representing the most recently used
+        """
+        if node == self.head:
+            return
+
+        if node == self.tail:
+            self.tail = node.prev
+        else:
+            node.next.prev = node.prev
+
+        node.prev.next = node.next
+        node.next = self.head
+        node.prev = None
+        self.head.prev = node
+        self.head = node
+
+
+class LruCacheNode:
+    """Creates a Node object for a Least Recently Used Cache.
+
+    Attributes:
+        key: An int representing the key the node is stored at in the cache
+        value: An int representing the value to store in the cache
+        prev: A LruCache Node object representing the previous node in a doubly
+            linked list
+        next: A LruCache Node object representing the next node in a doubly
+            linked list
+    """
+
+    def __init__(self, key, value, head):
+        """Set-up for a least recently used cache node."""
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = head
 
 
 def main():
     """Main function call to test the functionality of the LruCache."""
     our_cache = LruCache(5)
-
     our_cache.put(1, 1)
     our_cache.put(2, 2)
     our_cache.put(3, 3)
@@ -80,9 +133,20 @@ def main():
     assert our_cache.get(1) == 1
     assert our_cache.get(2) == 2
     assert our_cache.get(9) == -1
-
     our_cache.put(5, 5)
     our_cache.put(6, 6)
+    assert our_cache.get(3) == -1
+
+    our_cache = LruCache(3)
+    our_cache.put(1, 1)
+    our_cache.put(2, 2)
+    our_cache.put(3, 3)
+    our_cache.put(4, 4)
+    assert our_cache.get(4) == 4
+    assert our_cache.get(1) == -1
+    our_cache.put(2, 4)
+    assert our_cache.get(2) == 4
+    our_cache.put(5, 5)
     assert our_cache.get(3) == -1
 
     print("All test cases passed!")
